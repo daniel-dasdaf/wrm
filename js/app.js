68c77c424be044e0925212426cedd3c2419c61a0
daniel-dasdaf <106155653+daniel-dasdaf@users.noreply.github.com>
2023-10-22 22:09:03 +0100

    https for everylink.

--- js/app.js
+++ js/app.js
@@ -112,9 +112,9 @@ document.getElementById('checkstatus').addEventListener('input', (e) => {
 
 })
 
-L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 function addMarker(e){
